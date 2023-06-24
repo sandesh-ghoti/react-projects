6e76d1f4b34a2db4e9ec798fba198c469e956827
sandesh-ghoti <sandesh.ghoti@gmail.com>
2023-06-24 15:08:58 +0530

    commit-1

---
+++ react-with-tailwind/app1-tailwind/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl text-center font-bold capitalize bg-slate-800 text-white">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
